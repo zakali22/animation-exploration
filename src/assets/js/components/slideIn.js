c99a835a90544d90e81955ed00fea19b5b1d6b45
Zakeria Ali <zak.fined@gmail.com>
2023-02-18 23:09:07 +0000

    Add pinned panels animation concept

---
+++ src/assets/js/components/slideIn.js
@@ -0,0 +1,30 @@
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
+
+const slideIn = (function(){
+    function init(){
+        const slideInEls = document.querySelectorAll('.slideIn')
+        slideInEls.forEach(slideInEl => {
+            gsap.set(slideInEl, {visibility: "hidden"})
+            gsap.fromTo(slideInEl, {y: 60, autoAlpha: 0}, {y: 0, autoAlpha: 1, duration: 1, scrollTrigger: {
+                trigger: slideInEl,
+                start: "top center"
+            }})
+
+            // new ScrollTrigger.create({
+            //     trigger: slideInEl, 
+            //     onEnter: function(){
+            //         console.log("Slide in " + slideInEl)
+            //     }
+            // })
+        })
+    }
+
+    return {
+        init
+    }
+})()
+
+slideIn.init()
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,6 @@
-"use strict";
-
-require("../scss/index.scss");
-require("./components/theme-toggle.js");
-require("./components/customCursor.js");
-require("./components/headerReveal.js");
+import "../scss/index.scss"
+import "./components/slideIn.js"
+import "./components/theme-toggle.js"
+import "./components/customCursor.js"
+import "./components/headerReveal.js"
+import "./components/pinnedPanels.js"
